584d3c60489962f02bef4dde5c6c84e5f022fdc2
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-07-09 16:13:04 +0430

    Add project files.

---
+++ Food/Controllers/FoodController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FoodController : ControllerBase
+    {
+
+    }
+    
+}
